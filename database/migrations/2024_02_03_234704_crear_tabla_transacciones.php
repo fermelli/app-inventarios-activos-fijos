16fc70a4b76c69480f77e74fe2731cec44791a8b
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-03-01 19:06:27 -0400

    modificado migracion para que sea mas mantenible

--- database/migrations/2024_02_03_234704_crear_tabla_transacciones.php
+++ database/migrations/2024_02_03_234704_crear_tabla_transacciones.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Transaccion;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -18,7 +17,7 @@ return new class extends Migration
             $table->unsignedBigInteger('institucion_id');
             $table->date('fecha');
             $table->enum('tipo', ['entrada']);
-            $table->enum('comprobante', Transaccion::COMPROBANTES);
+            $table->enum('comprobante', ['factura', 'recibo', 'nota', 'boleta', 'otro']);
             $table->string('numero_comprobante', 100);
             $table->string('observacion', 255)->nullable();
             $table->timestamp('creado_en');
