eb5843105620d20f7f8caa0c7ea2f01e78bdc3c9
fermelli <luisfernandosalgadomiguez@gmail.com>
2024-10-31 01:56:21 -0400

    agregar respuestas personalizadas para login logout y registro en Fortify

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -7,6 +7,9 @@ use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Fortify\Contracts\LoginResponse;
+use Laravel\Fortify\Contracts\LogoutResponse;
+use Laravel\Fortify\Contracts\RegisterResponse;
 use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
@@ -16,6 +19,32 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->instance(LogoutResponse::class, new class implements LogoutResponse {
+            public function toResponse($request)
+            {
+                return $request->wantsJson()
+                    ? response()->jsonResponse('Usuario desautenticado.', null, 200)
+                    : redirect()->intended('/');
+            }
+        });
+
+        $this->app->instance(RegisterResponse::class, new class implements RegisterResponse {
+            public function toResponse($request)
+            {
+                return $request->wantsJson()
+                    ? response()->jsonResponse('Usuario registrado.', null, 201)
+                    : redirect()->intended('/');
+            }
+        });
+
+        $this->app->instance(LoginResponse::class, new class implements LoginResponse {
+            public function toResponse($request)
+            {
+                return $request->wantsJson()
+                    ? response()->jsonResponse('Usuario autenticado.', $request->user(), 200)
+                    : redirect()->intended('/');
+            }
+        });
     }
 
     /**
